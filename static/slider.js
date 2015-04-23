d30994307095e89887a2e823c23c157fdd79315f
Gregor Aisch <gregor.aisch@nytimes.com>
2015-04-23 10:19:53 -0400

    showing value in slider

--- static/slider.js
+++ static/slider.js
@@ -3,12 +3,15 @@ $(function() {
 
     function syncSlider(args) {
         var curVal = args.chart.get('metadata.visualize.'+args.key, args.option.default),
-            input = $('#'+args.key);
+            input = $('#'+args.key),
+            display = $('.value', input.parent());
 
         input.val(curVal);
+        display.html(curVal);
 
         input.on('change', function() {
             args.chart.set('metadata.visualize.'+args.key, input.val());
+            display.html(input.val());
         });
     }
 
