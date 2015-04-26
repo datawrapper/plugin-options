be293bf53a66ba6009507facef19793f17614407
Gregor Aisch <gregor.aisch@nytimes.com>
2015-04-25 23:53:42 -0400

    switched to moment.js for date formatting

--- static/custom-format.js
+++ static/custom-format.js
@@ -8,6 +8,14 @@ $(function() {
             colid = _.isArray(colids) ? colids[0] : colids,
             axesCol = colid ? args.chart.dataset().column(colid) : null;
 
+        var formatMap = {
+            '%Y': 'YYYY',
+            '’%y': '’YY',
+            '%B': 'MMMM',
+            '%b': 'MMM',
+            '%b ’%y': 'MMM ’YY'
+        };
+
         if (!colid) return;
         if (axesCol.type() == 'number') {
             formats = [
@@ -25,15 +33,21 @@ $(function() {
             ];
         } else if (axesCol.type() == 'date') {
             formats = [
-                { l: '2015', f: '%Y' },
-                { l: '’15', f: '’%y' },
-                { l: 'April', f: '%B' },
-                { l: 'Apr', f: '%b' },
-                { l: 'Apr ’15', f: '%b ’%y' }
+                { l: '2015', f: 'YYYY' },
+                { l: '2015 Q1', f: 'YYYY [Q]Q' },
+                { l: '’15', f: '’YY' },
+                { l: 'April', f: 'MMMM' },
+                { l: 'Apr', f: 'MMM' },
+                { l: 'Apr ’15', f: 'MMM ’YY' }
             ];
         }
 
         var select = d3.select('#'+args.key).html('');
+
+        if (formatMap[curVal]) {
+            curVal = formatMap[curVal];
+            args.chart.set('metadata.visualize.'+args.key, curVal);
+        }
 
         select.selectAll('option')
             .data(formats)
