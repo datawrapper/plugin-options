48aeb30f8cbd8cdd11ccfc1bbb90f05d8415a0f6
gka <mail@driven-by-data.net>
2015-08-14 21:07:10 -0400

    added new typeahead option!

--- init.php
+++ init.php
@@ -2,8 +2,8 @@
 
 global $app;
 
-$options = array('custom-format', 'custom-format-numeral', 'slider', 'axis-matrix');
-$assets = array('d3.min.js', 'd3-jetpack.min.js');
+$options = array('custom-format', 'custom-format-numeral', 'slider', 'axis-matrix', 'typeahead');
+$assets = array('vendor/typeahead.jquery.js');
 
 foreach ($options as $opt) {
     $assets[] = $opt.'.js';
---
+++ static/typeahead.js
@@ -0,0 +1,38 @@
+
+$(function() {
+
+    var substringMatcher = function(items) {
+
+        return function findMatches(q, cb) {
+            var matches = [],
+                qRegex = new RegExp(q.split('').join('.*?'), 'i');
+
+            $.each(items, function(i, item) {
+                if (qRegex.test(item.label)) matches.push(item);
+            });
+
+            cb(matches);
+        };
+    };
+
+
+    function syncTypeahead(args) {
+        var curVal = args.chart.get('metadata.visualize.'+args.key, args.option.default),
+            input = $('#'+args.key);
+
+        input.typeahead2({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },{
+            name: 'states',
+            source: substringMatcher(args.option.options),
+            display: 'label'
+        }).on('typeahead:select', function(ev, suggestion) {
+            args.chart.set('metadata.visualize.'+args.key, suggestion.value);
+        }).typeahead2('val', _.find(args.option.options, { value: curVal }).label);
+    }
+
+    dw.backend.on('sync-option:typeahead', syncTypeahead);
+
+});
